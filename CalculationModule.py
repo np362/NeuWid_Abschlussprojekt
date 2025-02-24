78168a4ddbda2c26f033b706ef95216b1d2ae3c6
FloWidmann <flo.widmann07@outlook.com>
2025-02-24 16:44:29 +0100

    circle is now a singleton and added a few methods

--- CalculationModule.py
+++ CalculationModule.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+from scipy.optimize import minimize
 from typing import Tuple
 import math
 
@@ -9,48 +10,88 @@ import math
 P = nx.Graph() #one Graph is for real joints and connections, the other is for theoretical ones such as center points and radius 
 C = nx.Graph() 
 
-class Center():
-    """Since we only want one rotation point, maybe consider a Singleton?"""
-   
-    def __init__(self, name: str, posX: int, posY: int):
-        self.name = name
-        self.posX = posX
-        self.posY = posY
-        C.add_node(self.name, pos=(self.posX, self.posY))
+class Center:
+    """Since we only want one rotation point, a Singleton is used here"""
+    _instance = None 
+
+    def __new__(cls, name: str, posX: int, posY: int, rotatingPoint, angle : float = None, radius : int = None):
+        if cls._instance is None:
+            cls._instance = super(Center, cls).__new__(cls)
+            cls._instance.name = name
+            cls._instance.posX = posX
+            cls._instance.posY = posY
+            cls._instance.radius = radius
+            cls._instance.rotatingPoint = rotatingPoint
+            C.add_node(cls._instance.name, pos=(cls._instance.posX, cls._instance.posY))
+            
+            if C.degree(cls._instance.name) <= 1:
+                C.add_edge(cls._instance.name, rotatingPoint.name)
+                C.nodes[rotatingPoint.name]['pos'] = (rotatingPoint.posX, rotatingPoint.posY)
+            else:
+                print("Kann nicht mehr als ein Knoten hinzugefügt werden")
+            
+        return cls._instance
     
-    def add_rotating_point(self, point):
-        if C.degree(self.name) <= 1:
-            C.add_edge(self.name, point.name)
-        else:
-            print("Kann nicht mehr als ein Knoten hinzugefügt werden")
+    def __init__(self, name: str, posX: int, posY: int, rotatingPoint, angle : float = None, radius : int = None ):
+        if not hasattr(self, 'initialized'):
+            self.name = name
+            self.posX = posX
+            self.posY = posY
+            self.radius = radius
+            self.angle = angle
+            self.rotatingPoint = rotatingPoint
+            
+            C.add_node(self.name, pos=(self.posX, self.posY))
+            self.initialized = True
+    
 
     def rotate_point(self, degree: float):
-        neighbors = list(C.neighbors(self.name))  # Gets the connected points
-        if neighbors:
-            neighbor = neighbors[0]  # Take the only neighbor
-            neighborPos = C.nodes[neighbor]['pos']  # Gets the position of the connected point
-            distance = math.sqrt((self.posX - neighborPos[0]) ** 2 + (self.posY - neighborPos[1]) ** 2)
-            print(f"Distanz von {self.name} zu {neighbor}: {distance}")
-        else:
-            print(f"{self.name} hat keinen Nachbarn")
+        if self.angle is None:
+            self.angle = math.atan2(self.rotatingPoint.posY - self.posY, self.rotatingPoint.posX - self.posX)
 
-class Point(Center):
+        if self.radius is None:
+            self.radius = math.sqrt((self.posX - self.rotatingPoint.posX) ** 2 + (self.posY - self.rotatingPoint.posY) ** 2)
+
+        # Convert degree to radians
+        rad = math.radians(degree)
+        # Calculate new angle
+        newAngle = self.angle + rad
+        # Calculate new position
+        newPosX = self.posX + self.radius * math.cos(newAngle)
+        newPosY = self.posY + self.radius * math.sin(newAngle)
+        # Update position of rotatingPoint
+        self.rotatingPoint.update_position(newPosX, newPosY)
+        # Update the angle
+        self.angle = newAngle
+
+                
+        
+    
+
+class Point():
     """These are points that are not center of a circle"""
     allPoints = [] #creates a list which is shared between all objects
 
     def __init__(self, name: str, posX: int, posY: int, isFixed: bool):
-        super().__init__(name, posX, posY)
-    
+        
+        self.name = name
+        self.posX = posX
+        self.posY = posY
         self.isFixed = isFixed
         self.vec = np.array([[self.posX], [self.posY]])
         self.connectedPoints = []
         Point.allPoints.append(self) #adds Point object to this list
         P.add_node(self.name, pos=(self.posX, self.posY))
+    
+    def get_position(self):
+        return self.posX, self.posY
 
     def update_position(self, newPosX, newPosY):
-        self.posX = newPosX
-        self.posY = newPosY
-        P.nodes[self.name]['pos'] = (self.posX, self.posY)
+        if self.isFixed == False:
+            self.posX = newPosX
+            self.posY = newPosY
+            P.nodes[self.name]['pos'] = (self.posX, self.posY)
+            self.vec = np.array([[self.posX], [self.posY]])  # Update the vec attribute
 
     def add_connection(self, *points):
         for point in points:
@@ -64,18 +105,16 @@ class Point(Center):
 
 class Calculation:
 
-    xVec = np.empty((0, 0))  # Empty array with 0 rows and 0 columns
-    AMatrix = np.empty((0,0))
-    LVec = np.empty((0,0))
-    lVec = np.empty((0,1)) #Initialize with 1 column, so you can add 
+    lVecList = []
 
     @classmethod
     def create_xVec(cls, points: list):
+        cls.xVec = np.empty((0,0))
         """This creates a column vector similar to x in script"""
         for point in points:
             vecs = [point.vec for point in points]
             cls.xVec = np.vstack(vecs)
-        print(cls.xVec)
+        print(f"xVec:\n {cls.xVec}")
 
     @classmethod
     def create_AMatrix(cls, points: list):
@@ -96,54 +135,99 @@ class Calculation:
             cls.AMatrix[row, nodeIndices[end] * 2] = -1
             cls.AMatrix[row+1, nodeIndices[end] * 2 + 1] = -1
             row += 2
-        #print(cls.AMatrix)
+        print(f"AMatrix:\n {cls.AMatrix}")
     
     @classmethod
     def create_lVec(cls):
-        cls.LVec = cls.AMatrix @ cls.xVec #care about order, matrices are not commutative
-        #print(cls.LVec)
-        # Converts LVec into a 2-column vector
+        # Reset the lVec attribute
+        cls.lVec = np.empty((0,1))  # Initialize with 1 column, so you can add
+        cls.LVec = np.empty((0,0))
+        # Recalculate LVec
+        cls.LVec = cls.AMatrix @ cls.xVec  # Care about order, matrices are not commutative
         numRows = cls.LVec.shape[0] // 2
         cls.LVec = cls.LVec.reshape(numRows, 2)
-        #print(cls.LVec)
+        print(f"LVec:\n {cls.LVec}")
 
-        for i in range(0, int(cls.LVec.shape[0])-1):
-            #calculating the distance between the two points
-            numVecX = np.array([math.sqrt(cls.LVec[i,0]**2 + cls.LVec[i+1,0]**2)])
+        for i in range(0, int(cls.LVec.shape[0])-1): 
+            # Calculating the distance between the two points 
+            numVecX = np.array([math.sqrt(cls.LVec[i,0]**2 + cls.LVec[i, 1]**2)]) 
             cls.lVec = np.vstack((cls.lVec, numVecX))
-            numVecY = np.array([math.sqrt(cls.LVec[i,1]**2 + cls.LVec[i+1,1]**2)])
             
-            cls.lVec = np.vstack((cls.lVec, numVecY))
-        #print(cls.lVec)
+            numVecY = np.array([math.sqrt(cls.LVec[i+1,0]**2 + cls.LVec[i+1,1]**2)]) 
+            cls.lVec = np.vstack((cls.lVec, numVecY)) 
+
+        print(f"lVec:\n {cls.lVec}")
+        if len(cls.lVecList) >= 2:
+            cls.lVecList.pop(0)
+        cls.lVecList.append(cls.lVec)
+
+    @classmethod
+    def calculate_error(cls):
+        if len(cls.lVecList) >= 2:
+            cls.eVec = np.empty((0,0))
+            firstlVec = cls.lVecList[0]
+            secondlVec = cls.lVecList[1]
+            differences = secondlVec - firstlVec
+            cls.eVec = differences
+            print(f"Differences:\n{cls.eVec}")
+
+            def error_function(params):
+                new_positions = params.reshape((-1, 2))
+                total_error = np.sum(np.abs(new_positions - cls.eVec))
+                return total_error
+
+            initial_guess = cls.eVec.flatten()
+            result = minimize(error_function, initial_guess, method='BFGS')
+            optimized_params = result.x.reshape(cls.eVec.shape)
+            print(f"Optimized parameters: \n{optimized_params}")
+            
+        else:
+            print("Not enough lVecs in lVecList to calculate differences.")
+            
+            
+
         
+
+
+            
+
 
 p0Vec = Point("A",0, 0, True)
 p1Vec = Point("B",10, 35, False)
 p2Vec = Point("C",-25, 10, False)
+centerVec = Center("center", -30, 0, p2Vec,angle = None, radius=None)
 
-centerVec = Center("center", -30, 0)
-centerVec.add_rotating_point(p2Vec)
+p0Vec.add_connection(p1Vec)
+p1Vec.add_connection(p2Vec)
 
 
 Calculation.create_xVec(Point.allPoints)
-p0Vec.add_connection(p1Vec)
-p1Vec.add_connection(p2Vec)
 Calculation.create_AMatrix(Point.allPoints)
 Calculation.create_lVec()
+
 centerVec.rotate_point(10)
+print("\n Winkel erhöht \n")
 
+Calculation.create_xVec(Point.allPoints)
+Calculation.create_AMatrix(Point.allPoints)
+Calculation.create_lVec()
 
+Calculation.calculate_error()
+
+"""
 def update(num, p0Vec):
 
     ax.clear()
-    p0Vec.update_position(p0Vec.posX + 1, p0Vec.posY)
+    centerVec.rotate_point(1)
+    #p0Vec.update_position(p0Vec.posX+1, p0Vec.posY)
     pos = (nx.get_node_attributes(C, 'pos'))
     pos.update(nx.get_node_attributes(P, 'pos'))
-    nx.draw(C, pos, with_labels=True, node_size=700, node_color="red", font_size=10, font_weight="bold", edge_color="red", style="dashed")
-    nx.draw(P, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=10, font_weight="bold")
+    nx.draw(C, pos, with_labels=True, node_size=350, node_color="red", font_size=10, font_weight="bold", edge_color="red", style="dashed")
+    nx.draw(P, pos, with_labels=True, node_size=350, node_color="skyblue", font_size=10, font_weight="bold")
     
 
 fig, ax = plt.subplots()
-ani = animation.FuncAnimation(fig, update, frames=range(20), fargs=(p0Vec,), interval=200, repeat=False)
+ani = animation.FuncAnimation(fig, update, frames=range(800), fargs=(p0Vec,), interval=300, repeat=False)
 
 plt.show()
+"""
--- calculations.py
+++ calculations.py
@@ -1,36 +1,119 @@
-"""This module contains all the calculations for the application."""
-
+import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import pandas as pd
-import scipy as sp
+import math
+from typing import Tuple
 
-class Mechanism():
 
-    def __init__(self):
-        self.matrix = None
-        self.connections = None
-        self.vector = None
+class Point:
+    def __init__(self, posX: int, posY: int, isFixed: bool):
+        self.posX = posX
+        self.posY = posY
+        self.isFixed = isFixed
+        self.vec = np.array([[self.posX], [self.posY]])
+        self.connectedPoints = []
 
-    def calculate_track(self, *args):
-        pass
+    def draw_point(self):
+        print(self.vec)
 
-    def Are_points_connected(self, point1, point2):
-        pass
+    def update_position(self, newPosX, newPosY):
+        self.posX = newPosX
+        self.posY = newPosY
 
-    @classmethod
-    def calculate_matrix(cls, vec, FT_points):
-        A = np.zeros([len(vec)-2, len(vec)])
-        for state in range(len(FT_points)):
-            if FT_points[state]:
-                A[state][state] = 1
-                A[state][state+2] = -1
-        print(A)
-        x = vec.T
-        return A*x
+    def add_connected_point(self, point):
+        if isinstance(point, Point) and point not in self.connectedPoints: #Adds a Point to the connectedPoint-list, if it´s not itself
+            self.connectedPoints.append(point)
 
-if __name__ == "__main__":
-    rechnung = Mechanism()
-    punkte = np.array([1, 2, 3, 3, 2, 1])
-    statements = np.array([True, False, True])
-    rechnung.calculate_matrix(punkte, statements)
+    def display_connected_points(self):
+        if not self.connectedPoints:
+            print("No connected points")
+        else:
+            for point in self.connectedPoints:
+                print(f'Connected point at ({point.posX}, {point.posY})')
+
+
+class Calculation:
+    points = []
+    pointPairs = []
+
+    @staticmethod
+    def add_points(*points: Point):
+        for point in points:
+            if isinstance(point, Point):
+                Calculation.points.append(point)
+
+    @staticmethod
+    def add_point_pair(point_pair: Tuple[Point, Point]):
+        point1, point2 = point_pair
+        if isinstance(point1, Point) and isinstance(point2, Point):
+            Calculation.pointPairs.append(point_pair)
+
+    @staticmethod
+    def display_points():
+        if not Calculation.points:
+            print("Keine Punkte hinzugefügt")
+        else:
+            for point in Calculation.points:
+                print(f'Point at ({point.posX}, {point.posY}), Fixed: {point.isFixed}')
+
+    @staticmethod
+    def display_point_pairs():
+        if not Calculation.pointPairs:
+            print("Keine Punktpaare hinzugefügt")
+        else:
+            for point1, point2 in Calculation.pointPairs:
+                print(f'Pair: Point 1 at ({point1.posX}, {point1.posY}), Point 2 at ({point2.posX}, {point2.posY})')
+
+# Beispielverwendung:
+p0Vec = Point(0, 0, True)
+p1Vec = Point(10, 35, True)
+p2Vec = Point(-25, 10, True)
+p3Vec = Point(15, 20, False)
+
+Calculation.add_points(p0Vec, p1Vec, p2Vec)
+Calculation.add_point_pair((p0Vec, p1Vec))
+Calculation.add_point_pair((p2Vec, p3Vec))
+
+
+p0Vec = np.array([[0], [0]])
+p1Vec = np.array([[10], [30]])
+p2Vec = np.array([[-25], [10]])
+
+xVec = np.vstack((p0Vec, p1Vec, p2Vec))
+print(xVec)
+
+cVec = np.array([[-30], [0]])
+# Matrix 2m x 2n, m-Points, n-rods
+                #p0-x,y  p1-x.y
+AMatrix = np.array([[1,0,-1,0,0,0], #connection of p0 to p1
+                    [0,1,0,-1,0,0],
+                    [0,0,1,0,-1,0], #connection of p1 to p2
+                    [0,0,0,1,0,-1]])
+
+diffVec = AMatrix @ xVec
+
+# lVec umwandeln in die gewünschte Form
+LMatrix = np.array([[diffVec[0,0], diffVec[1,0]],
+                    [diffVec[2,0], diffVec[3,0]]])
+
+lVec = np.empty((0,1))
+
+
+
+for i in range(0, int(LMatrix.shape[0])-1):
+    #calculating the distance between the two points
+    print(f"{LMatrix[i,0]} {LMatrix[i+1,0]}")
+
+    numVec = np.array([math.sqrt(LMatrix[i,0]**2 + LMatrix[i+1,0]**2)])
+    lVec = np.vstack((lVec, numVec))
+    numVec = np.array([math.sqrt(LMatrix[i,1]**2 + LMatrix[i+1,1]**2)])
+    lVec = np.vstack((lVec, numVec))
+   
+
+
+print(lVec)
+
+# Ausgabe der neuen Matrix
 
